9eb5c54e39df01b3134a847fd7c65fb6a2ed06e6
20121BSI0236 <phillipe.alcantara.fonseca@gmail.com>
2014-02-11 17:28:06 -0200

    Criação do método obter() na AplNotificacao();

--- src/main/java/br/ifes/leds/sincap/gerenciaNotificacao/cln/cgt/AplNotificacao.java
+++ src/main/java/br/ifes/leds/sincap/gerenciaNotificacao/cln/cgt/AplNotificacao.java
@@ -1,6 +1,5 @@
 package br.ifes.leds.sincap.gerenciaNotificacao.cln.cgt;
 
-
 import br.ifes.leds.sincap.gerenciaNotificacao.cgd.CausaObitoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +13,7 @@ import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.Obito;
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.Paciente;
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.Responsavel;
 import java.util.Calendar;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -27,63 +27,63 @@ public class AplNotificacao {
 
     @Autowired
     private NotificacaoRepository notificacaoRepository;
-    
+
     @Autowired
     private ObitoRepository obitoRepository;
-    
+
     @Autowired
     private PacienteRepository pacienteRepository;
-    
+
     @Autowired
     private ResponsavelRepository responsavelRepository;
-    
+
     @Autowired
     private CausaObitoRepository causaObitoRepository;
-  
-    public void salvar (Notificacao notificacao)
-    {
-        
-        
-        Obito obito = notificacao.getObito();        
-        salvarObito(obito);
-        
-        //Gerando o codigo
-        notificacao.setCodigo(genereateCode());        
-        notificacao.setDataNotificacao(Calendar.getInstance());
-        notificacaoRepository.save(notificacao);        
+
+    public List<Notificacao> obter() {
+        return notificacaoRepository.findAll();
     }
     
-    private void salvarObito (Obito obito)
-    {
+    public Obito getObito(Long id) {
+        return obitoRepository.findOne(id);
+    }
+
+    public void salvar(Notificacao notificacao) {
+
+        Obito obito = notificacao.getObito();
+        salvarObito(obito);
+
+        //Gerando o codigo
+        notificacao.setCodigo(genereateCode());
+        notificacao.setDataNotificacao(Calendar.getInstance());
+        notificacaoRepository.save(notificacao);
+    }
+
+    private void salvarObito(Obito obito) {
         //Salvando o paciente
         Paciente paciente = obito.getPaciente();
         salvarPaciente(paciente);
-        
+
         causaObitoRepository.save(obito.getPrimeiraCausaMortis());
         causaObitoRepository.save(obito.getSegundaCausaMortis());
         causaObitoRepository.save(obito.getTerceiraCausaMortis());
         causaObitoRepository.save(obito.getQuartaCausaMortis());
-        
+
         //Salvando o Obito
         obitoRepository.save(obito);
-        
-        
-    }
-    
-    private void salvarPaciente (Paciente paciente)
-    {
-        //Salvando responsavel
-        Responsavel responsavel = paciente.getResponsavel();        
-        responsavelRepository.save(responsavel);        
-        pacienteRepository.save(paciente);
-        
-        
-    }
-    
-    private String genereateCode()
-    {
-        return  UUID.randomUUID().toString();
+
     }
 
+    private void salvarPaciente(Paciente paciente) {
+        //Salvando responsavel
+        Responsavel responsavel = paciente.getResponsavel();
+        responsavelRepository.save(responsavel);
+        pacienteRepository.save(paciente);
+
+    }
+
+    private String genereateCode() {
+        return UUID.randomUUID().toString();
+    }
 
 }
