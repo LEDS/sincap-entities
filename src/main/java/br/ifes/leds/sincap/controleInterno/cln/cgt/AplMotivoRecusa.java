a98afc081717e596f6e10f917af436eb82761f9b
20121BSI0236 <phillipe.alcantara.fonseca@gmail.com>
2014-02-13 08:42:25 -0200

    Correção na busca pelo TipoMotivoRecusa

--- src/main/java/br/ifes/leds/sincap/controleInterno/cln/cgt/AplMotivoRecusa.java
+++ src/main/java/br/ifes/leds/sincap/controleInterno/cln/cgt/AplMotivoRecusa.java
@@ -14,36 +14,41 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class AplMotivoRecusa {
+
     @Autowired
     TipoMotivoRecusaRepository tipoMotivoRecusaRepository;
     @Autowired
     MotivoRecusaRepository motivoRecusaRepository;
-    
+
     // MOTIVO RECUSA
     public List<MotivoRecusa> obterTodosContraindicacaoMedica() {
         // 2 -- id do Contra Indicação Médica
         TipoMotivoRecusa tipo = this.obterTipoMotivoRecusa(2);
         return motivoRecusaRepository.findByTipoMotivoRecusa(tipo);
     }
-    
-    public MotivoRecusa salvar(MotivoRecusa motivoR){
+
+    public MotivoRecusa salvar(MotivoRecusa motivoR) {
         return motivoRecusaRepository.save(motivoR);
     }
-    
+
     // TIPO MOTIVO RECUSA
     public List<MotivoRecusa> obterTodosRecusaFamiliar() {
         // 1 -- id do Contra Indicação Médica
-        TipoMotivoRecusa tipo = this.obterTipoMotivoRecusa(1);
+        TipoMotivoRecusa tipo = this.obterTipoMotivoRecusa("Recusa Familiar");
         return motivoRecusaRepository.findByTipoMotivoRecusa(tipo);
     }
-    
-    public TipoMotivoRecusa salvar(TipoMotivoRecusa tipoMotivoR){
+
+    public TipoMotivoRecusa salvar(TipoMotivoRecusa tipoMotivoR) {
         return tipoMotivoRecusaRepository.save(tipoMotivoR);
     }
-    
+
     // All    
-    public TipoMotivoRecusa obterTipoMotivoRecusa(long id){
+    public TipoMotivoRecusa obterTipoMotivoRecusa(long id) {
         return tipoMotivoRecusaRepository.findOne(id);
+    }
+
+    public TipoMotivoRecusa obterTipoMotivoRecusa(String tipoMotivoRecusa) {
+        return tipoMotivoRecusaRepository.findByNome(tipoMotivoRecusa);
     }
 
     public MotivoRecusa obter(long id) {
