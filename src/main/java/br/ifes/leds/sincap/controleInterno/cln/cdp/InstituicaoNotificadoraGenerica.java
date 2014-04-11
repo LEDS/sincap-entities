555bba9ead5887b889dfd07a2759e9472693a995
20121BSI0236 <phillipe.alcantara.fonseca@gmail.com>
2014-04-10 21:29:05 -0300

    Criação das classes necessárias para o CRUD de Instituições notificadoras que não são hospitais. 	new file:   src/main/java/br/ifes/leds/sincap/controleInterno/cgd/InstituicaoNotificadoraGenericaRepository.java 	new file:   src/main/java/br/ifes/leds/sincap/controleInterno/cln/cdp/InstituicaoNotificadoraGenerica.java 	modified:   src/main/java/br/ifes/leds/sincap/controleInterno/cln/cgt/AplHospital.java 	new file:   src/main/java/br/ifes/leds/sincap/controleInterno/cln/cgt/AplInstituicaoNotificadoraGenerica.java

---
+++ src/main/java/br/ifes/leds/sincap/controleInterno/cln/cdp/InstituicaoNotificadoraGenerica.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.ifes.leds.sincap.controleInterno.cln.cdp;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Phillipe Lopes
+ */
+@Entity
+public class InstituicaoNotificadoraGenerica extends InstituicaoNotificadora {
+    
+}
