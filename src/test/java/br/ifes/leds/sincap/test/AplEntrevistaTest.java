cc18cf78a7bb0c2b9329e4f86f56254e54748e72
Breno Sampaio Grillo <bsampaio@outlook.com>
2014-07-01 17:02:13 -0300

    Início do desenvolvimento do teste da AplEntrevista.

---
+++ src/test/java/br/ifes/leds/sincap/test/AplEntrevistaTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.ifes.leds.sincap.test;
+
+import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.DTO.EntrevistaDTO;
+import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.Entrevista;
+import br.ifes.leds.sincap.gerenciaNotificacao.cln.cgt.AplEntrevista;
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author Breno Grillo
+ */
+public class AplEntrevistaTest extends AbstractionTest{
+    
+    @Autowired
+    private AplEntrevista aplEntrevista;
+    
+    //Preencher um DTO e uma Entrevista
+    @Before
+    public void before()
+    {
+        Entrevista entrevista = new Entrevista();
+    }
+}
