0e76c1b76562fa54c751e21eaa69dfdaf4029954
Lucas Possatti <lucas_possatti@hotmail.com>
2014-07-10 13:42:28 -0300

    Melhoramento na classe de utilidades
    Um novo método necessário foi criado, que retornasse a data e a hora
    formatados. E o código foi redesenhado para usar constantes para as
    strings de formatação.

--- src/main/java/br/ifes/leds/reuse/utility/Utility.java
+++ src/main/java/br/ifes/leds/reuse/utility/Utility.java
@@ -22,6 +22,10 @@ public enum Utility {
 
     INSTANCE;
 
+    private static final String FORMATO_DATA_HORA = "dd/MM/yyyy HH:mm";
+    private static final String FORMATO_DATA = "dd/MM/yyyy";
+    private static final String FORMATO_HORA = "HH:mm";
+
     private final Mapper mapper = DozerBeanMapperSingletonWrapper.getInstance();
 
     public static Utility getInstance() {
@@ -40,27 +44,35 @@ public enum Utility {
      * @param hora
      *            se refere ao valor de DTO.hora
      */
+    @Deprecated
     public void calendarToString(Calendar calendar, String data, String hora) {
-        SimpleDateFormat formatoHora = new SimpleDateFormat("HH:mm");
-        SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat formatoHora = new SimpleDateFormat(FORMATO_HORA);
+        SimpleDateFormat formatoData = new SimpleDateFormat(FORMATO_DATA);
         data = formatoData.format(calendar.getTime());
         hora = formatoHora.format(calendar.getTime());
     }
 
+    public String calendarToString(Calendar calendar) {
+
+        SimpleDateFormat formatoHora = new SimpleDateFormat(FORMATO_DATA_HORA);
+
+        return formatoHora.format(calendar.getTime());
+    }
+
     public String calendarHoraToString(Calendar hora) {
-        DateFormat formatHora = new SimpleDateFormat("HH:mm");
+        DateFormat formatHora = new SimpleDateFormat(FORMATO_HORA);
         return formatHora.format(hora.getTime());
     }
 
     public String calendarDataToString(Calendar hora) {
-        DateFormat formatData = new SimpleDateFormat("dd/MM/yyyy");
+        DateFormat formatData = new SimpleDateFormat(FORMATO_DATA);
         return formatData.format(hora.getTime());
     }
 
     public Calendar stringToCalendar(String data, String hora)
             throws ParseException {
         Calendar calendar = Calendar.getInstance();
-        DateFormat formatoDataHora = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        DateFormat formatoDataHora = new SimpleDateFormat(FORMATO_DATA_HORA);
         String dataHora = data + " " + hora;
         Date convertedDataHora = formatoDataHora.parse(dataHora);
         calendar.setTime(convertedDataHora);
@@ -69,7 +81,7 @@ public enum Utility {
 
     public Calendar stringToCalendar(String data) throws ParseException {
         Calendar calendar = Calendar.getInstance();
-        DateFormat formatoDataHora = new SimpleDateFormat("dd/MM/yyyy");
+        DateFormat formatoDataHora = new SimpleDateFormat(FORMATO_DATA);
         String dataHora = data;
         Date convertedDataHora = formatoDataHora.parse(dataHora);
         calendar.setTime(convertedDataHora);
