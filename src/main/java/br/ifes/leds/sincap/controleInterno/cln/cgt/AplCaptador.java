b5a0620e463f627721e40dd4756f23514f5de025
André Leão <andre.sp.leao@gmail.com>
2014-08-27 13:33:11 -0300

    Criação da Apl Captador.

--- src/main/java/br/ifes/leds/sincap/controleInterno/cln/cgt/AplCaptador.java
+++ src/main/java/br/ifes/leds/sincap/controleInterno/cln/cgt/AplCaptador.java
@@ -6,10 +6,56 @@
 
 package br.ifes.leds.sincap.controleInterno.cln.cgt;
 
+import br.ifes.leds.sincap.controleInterno.cgd.TelefoneRepository;
+import br.ifes.leds.sincap.controleInterno.cln.cdp.Captador;
+import org.springframework.beans.factory.annotation.Autowired;
+import br.ifes.leds.sincap.controleInterno.cgd.CaptadorRepository;
+import java.util.List;
+
 /**
  *
- * @author marcosdias
+ * @author André Leão
  */
 public class AplCaptador {
-    
+    @Autowired
+    private CaptadorRepository captadorRepository;
+    @Autowired
+    private TelefoneRepository telefoneRepository;
+
+    /** Método para salvar um captador.
+     * @param captador - objeto captador.
+     */
+    public void salvar(Captador captador) {
+        telefoneRepository.save(captador.getTelefone());
+        captadorRepository.save(captador);
+    }
+
+    /** Método para remover um captador pelo seu id.
+     * @param captador - id do captador.
+     */
+    public void exlcuir(Long captador) {
+        captadorRepository.delete(captador);
+    }
+
+    /** Método para obter um captador pelo seu cpf.
+     * @param captador - CPF do captador.
+     */
+
+    public Captador obter(String cpf) {
+        return captadorRepository.findByCpf(cpf);
+    }
+
+    /** Método para obter um captador pelo seu id.
+     * @param captador - id do captador.
+     */
+    public Captador obter(Long id) {
+        return captadorRepository.findOne(id);
+    }
+
+    /** Método para obter uma lista de captador existentes.
+     * @param captador - id do captador.
+     */
+    public List<Captador> obter() {
+        return captadorRepository.findAll();
+    }
 }
