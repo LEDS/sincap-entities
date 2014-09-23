064bef66c4f6112556a0c3d408e2ba72de668c91
André Leão <andre.sp.leao@gmail.com>
2014-09-23 11:34:43 -0300

    Metodo salvar na AplInstituicaoNotificadora

--- src/main/java/br/ifes/leds/sincap/controleInterno/cln/cgt/AplInstituicaoNotificadora.java
+++ src/main/java/br/ifes/leds/sincap/controleInterno/cln/cgt/AplInstituicaoNotificadora.java
@@ -1,6 +1,8 @@
 package br.ifes.leds.sincap.controleInterno.cln.cgt;
 
+import br.ifes.leds.reuse.endereco.cgd.EnderecoRepository;
 import br.ifes.leds.sincap.controleInterno.cgd.InstituicaoNotificadoraRepository;
+import br.ifes.leds.sincap.controleInterno.cgd.TelefoneRepository;
 import br.ifes.leds.sincap.controleInterno.cln.cdp.InstituicaoNotificadora;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +17,10 @@ public class AplInstituicaoNotificadora {
 
     @Autowired
     private InstituicaoNotificadoraRepository instituicaoNotificadoraRepository;
-    
+    @Autowired
+    private TelefoneRepository telefoneRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
     /**
      * Método responsável por buscar todas as intituições notificadoras.
      * @return Lista de todas instituições Notificadoras.
@@ -30,5 +35,15 @@ public class AplInstituicaoNotificadora {
      */
     public InstituicaoNotificadora obter(Long idInstituicao) {
         return instituicaoNotificadoraRepository.findOne(idInstituicao);
+    }
+
+    /**
+     * Método responsável por cadastrar uma instituicao no banco.
+     * @return Objeto instituição Notificadora.
+     */
+    public InstituicaoNotificadora salvar(InstituicaoNotificadora instituicaoNotificadora) {
+        enderecoRepository.save(instituicaoNotificadora.getEndereco());
+        telefoneRepository.save(instituicaoNotificadora.getTelefone());
+        return instituicaoNotificadoraRepository.save(instituicaoNotificadora);
     }
 }
