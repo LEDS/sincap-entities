19e67c1ef7fb0166623e6a1b2d7991ae30708af8
André Aguiar <andre.s.aguiar@hotmail.com>
2014-11-18 11:32:10 -0200

    Criação das classes para relatorios mensal do CIHDOTT

---
+++ src/main/java/br/ifes/leds/sincap/gerenciaNotificacao/cln/cdp/relatorios/ObitoCardio.java
@@ -0,0 +1,15 @@
+package br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.relatorios;
+
+
+// Classe que representa o relatorio de obito por causa de parada cardiorespiratoria.
+
+public class ObitoCardio
+{
+    Integer total;
+    Integer entrevistaRealizada;
+    Integer entrevistaNaoRealizada;
+    Integer consentimentoFamiliar;
+    Integer recusas;
+
+
+}
