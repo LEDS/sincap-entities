3de6434d47feffdd8ac45405f36b26c750080e12
Breno Grillo <brenogrillo@gmail.com>
2014-11-26 16:16:47 -0200

    Modificacoes nos modelos de responsavel e escolaridade

---
+++ src/main/java/br/ifes/leds/sincap/gerenciaNotificacao/cln/cdp/Escolaridade.java
@@ -0,0 +1,10 @@
+package br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp;
+
+/**
+ * Created by marcosdias on 29/09/14.
+ */
+public enum Escolaridade {
+    RG,
+    CARTEIRA_DE_TRABALHO,
+    PASSAPORTE;
+}
