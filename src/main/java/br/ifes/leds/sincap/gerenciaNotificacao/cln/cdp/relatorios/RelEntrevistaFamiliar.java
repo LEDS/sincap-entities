f7d8e4891efac9e9ca651fc64f869635a9cd377b
André Aguiar <andre.s.aguiar@hotmail.com>
2014-11-26 15:28:58 -0200

    Teste

--- src/main/java/br/ifes/leds/sincap/gerenciaNotificacao/cln/cdp/relatorios/RelEntrevistaFamiliar.java
+++ src/main/java/br/ifes/leds/sincap/gerenciaNotificacao/cln/cdp/relatorios/RelEntrevistaFamiliar.java
@@ -1,5 +1,15 @@
 package br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.relatorios;
 
+
+import lombok.*;
+import lombok.experimental.Builder;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
 public class RelEntrevistaFamiliar
 {
     Integer desconhecimento;
