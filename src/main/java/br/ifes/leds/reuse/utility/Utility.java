8b46ae36cd23cdd687eca7c4839271841fa412ee
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2015-02-03 13:33:47 -0200

    Métodos removidos acidentalmente adicionados denovo.

--- src/main/java/br/ifes/leds/reuse/utility/Utility.java
+++ src/main/java/br/ifes/leds/reuse/utility/Utility.java
@@ -16,7 +16,6 @@ import static java.util.Calendar.*;
 import static org.joda.time.Years.yearsBetween;
 
 /**
- *
  * @author 20112bsi0083
  */
 @Component
@@ -27,6 +26,70 @@ public class Utility {
     private static final String FORMATO_HORA = "HH:mm";
 
     private final Mapper mapper = DozerBeanMapperSingletonWrapper.getInstance();
+
+    /**
+     * @param calendar representa o a data salva na classe que precisa ser
+     *                 transformada em string para ser usada pelo DTO e
+     *                 posteriormente a interface
+     * @param data     se refere ao valor de DTO.data
+     * @param hora     se refere ao valor de DTO.hora
+     * @author 20112bsi0083
+     */
+    @Deprecated
+    public void calendarToString(Calendar calendar, String data, String hora) {
+        SimpleDateFormat formatoHora = new SimpleDateFormat(FORMATO_HORA);
+        SimpleDateFormat formatoData = new SimpleDateFormat(FORMATO_DATA);
+        data = formatoData.format(calendar.getTime());
+        hora = formatoHora.format(calendar.getTime());
+    }
+
+    public String calendarToString(Calendar calendar) {
+
+        SimpleDateFormat formatoHora = new SimpleDateFormat(FORMATO_DATA_HORA);
+
+        return formatoHora.format(calendar.getTime());
+    }
+
+    public String calendarHoraToString(Calendar hora) {
+        DateFormat formatHora = new SimpleDateFormat(FORMATO_HORA);
+        return formatHora.format(hora.getTime());
+    }
+
+    public String calendarDataToString(Calendar hora) {
+        DateFormat formatData = new SimpleDateFormat(FORMATO_DATA);
+        return formatData.format(hora.getTime());
+    }
+
+    public Calendar stringToCalendar(String data, String hora)
+            throws ParseException {
+        Calendar calendar = Calendar.getInstance();
+        DateFormat formatoDataHora = new SimpleDateFormat(FORMATO_DATA_HORA);
+        String dataHora = data + " " + hora;
+        Date convertedDataHora = formatoDataHora.parse(dataHora);
+        calendar.setTime(convertedDataHora);
+        return calendar;
+    }
+
+    public Calendar stringToCalendar(String data) throws ParseException {
+        Calendar calendar = Calendar.getInstance();
+        DateFormat formatoDataHora = new SimpleDateFormat(FORMATO_DATA);
+        String dataHora = data;
+        Date convertedDataHora = formatoDataHora.parse(dataHora);
+        calendar.setTime(convertedDataHora);
+        return calendar;
+    }
+
+    public int calculaIdade(Calendar dataNasc, Calendar dataFim) {
+        return yearsBetween(new DateTime(dataNasc), new DateTime(dataFim)).getYears();
+    }
+
+    public Long booleanToLong(boolean attribute) {
+        if (attribute) {
+            return (long) 1;
+        } else {
+            return (long) 0;
+        }
+    }
 
     /**
      * Pesquisa um objeto em uma lista baseado em um método.
@@ -49,10 +112,8 @@ public class Utility {
     /**
      * Mapeia uma lista de um tipo para outro.
      *
-     * @param listaOrigem
-     *            A lista a ser mapeada.
-     * @param classeDestino
-     *            O tipo destino a ser mapeado.
+     * @param listaOrigem   A lista a ser mapeada.
+     * @param classeDestino O tipo destino a ser mapeado.
      * @return Uma lista do tipo definido no {@code classeDestino}.
      */
     public <K, T> List<T> mapList(List<K> listaOrigem, Class<T> classeDestino) {
@@ -63,6 +124,54 @@ public class Utility {
         }
 
         return listaDestino;
+    }
+
+    /**
+     * Dado um objeto {@code source}, insere todos os atributos com nome "id" dessa classe (aninhados inclusive)
+     * dentro do objeto {@code destination}. É útil para edição de objetos no banco de dados.
+     *
+     * @param destination Objeto no qual os ids serão inseridos.
+     * @param source      Objeto do qual os ids vem.
+     * @param <T>         Tipo dos objetos que serão manipulados.
+     */
+    public <T> void mergeIds(T destination, T source) {
+        Method[] methods = source.getClass().getMethods();
+
+        for (Method getMethod : methods) {
+            if (enterMergeLoop(destination, getMethod)) {
+                String fromName = getMethod.getName();
+                String toName = fromName.replace("get", "set");
+
+                try {
+                    Method setMethod = source.getClass().getMethod(toName, getMethod.getReturnType());
+                    Object sourceValue = getMethod.invoke(source);
+
+                    if (sourceValue != null) {
+                        Object destinationValue = getMethod.invoke(destination);
+
+                        if (!stopRecursion(sourceValue)) {
+                            mergeIds(destinationValue, sourceValue);
+                        } else if (fromName.equals("getId") && destinationValue == null) {
+                            setMethod.invoke(destination, sourceValue);
+                        }
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private <T> boolean enterMergeLoop(T destination, Method getMethod) {
+        return getMethod.getName().startsWith("get") && !getMethod.getName().equals("getClass")
+                && destination != null;
+    }
+
+    private boolean stopRecursion(Object value) {
+        return value.getClass().isPrimitive() || value.getClass().isEnum() || value instanceof String
+                || Calendar.class.isAssignableFrom(value.getClass())
+                || value instanceof Long || Collection.class.isAssignableFrom(value.getClass());
     }
 
     public static Calendar hoje() {
