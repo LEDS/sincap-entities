c0a68daba739861a3d6467d12362094330588efe
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2015-02-12 01:29:22 -0200

    Atualização dos testes para utilizarem o H2

--- src/test/java/br/ifes/leds/sincap/test/AbstractionTest.java
+++ src/test/java/br/ifes/leds/sincap/test/AbstractionTest.java
@@ -2,6 +2,7 @@ package br.ifes.leds.sincap.test;
 
 
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:spring-context.xml")
 @Transactional
+@ActiveProfiles("dev")
 public abstract class AbstractionTest {
 
 }
