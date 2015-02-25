4874262dc5297ab339e935ea7407b70dfd8c8a63
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2015-02-25 01:48:54 -0300

    Correção do problema das datas.
    O problema é que o AOP (AspectJ) estava desativado,
    portanto o TimeZone renderizado no JSF ficava 3 horas
    adiantado.

---
+++ src/main/java/br/ifes/leds/config/SincapConfig.java
@@ -0,0 +1,10 @@
+package br.ifes.leds.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class SincapConfig {
+
+}
