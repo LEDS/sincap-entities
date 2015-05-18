fecc76b588fcd9da8de5c2edb3b1590f1ba30528
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2015-05-18 15:55:19 -0300

    Pequena refatoração.

--- src/main/java/br/ifes/leds/sincap/gerenciaNotificacao/cln/cgt/AplObito.java
+++ src/main/java/br/ifes/leds/sincap/gerenciaNotificacao/cln/cgt/AplObito.java
@@ -8,7 +8,6 @@ import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.Paciente;
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.ProcessoNotificacao;
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.dto.ObitoDTO;
 import br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.dto.PacienteDTO;
-import br.ifes.leds.sincap.validacao.groups.entrevista.EntrevistaNaoRealizada;
 import br.ifes.leds.sincap.validacao.groups.obito.*;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +19,9 @@ import javax.validation.Validation;
 import javax.validation.Validator;
 import java.util.List;
 import java.util.Set;
+
+import static br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.CorpoEncaminhamento.NAO_ENCAMINHADO;
+import static br.ifes.leds.sincap.gerenciaNotificacao.cln.cdp.TipoDocumentoComFoto.PNI;
 
 @Service
 public class AplObito {
@@ -37,7 +39,7 @@ public class AplObito {
 
     /**
      * Obtém todos os pacientes.
-     * 
+     *
      * @return Uma lista de {@code PacienteDTO}.
      */
     public List<PacienteDTO> obterTodosPacientes() {
@@ -46,9 +48,8 @@ public class AplObito {
 
     /**
      * Obtém os dados de um paciente, dado seu id.
-     * 
-     * @param id
-     *            O {@code id} do paciente no banco de dados.
+     *
+     * @param id O {@code id} do paciente no banco de dados.
      * @return Um DTO de paciente.
      */
     public PacienteDTO obterPaciente(Long id) {
@@ -60,9 +61,8 @@ public class AplObito {
     /**
      * Salva os dados de um paciente, tanto para alteração quanto para um novo
      * paciente.
-     * 
-     * @param pacienteDTO
-     *            O objeto que representa o paciente.
+     *
+     * @param pacienteDTO O objeto que representa o paciente.
      */
     public void salvarPaciente(PacienteDTO pacienteDTO) {
         pacienteRepository.save(mapper.map(pacienteDTO, Paciente.class));
@@ -70,7 +70,7 @@ public class AplObito {
 
     /**
      * Obtém todos os obitos.
-     * 
+     *
      * @return Uma lista de {@code ObitoDTO}.
      */
     public List<ObitoDTO> obterTodosObitos() {
@@ -79,9 +79,8 @@ public class AplObito {
 
     /**
      * Salva os dados de um óbito, tanto para alteração quanto para um novo
-     * 
-     * @param obitoDTO
-     *            O objeto que representa o óbito.
+     *
+     * @param obitoDTO O objeto que representa o óbito.
      */
     public void salvarObito(ObitoDTO obitoDTO) {
         obitoRepository.save(mapper.map(obitoDTO, Obito.class));
@@ -91,30 +90,30 @@ public class AplObito {
         obitoRepository.save(obito);
     }
 
-    public static void validarObito(ProcessoNotificacao processo){
+    public static void validarObito(ProcessoNotificacao processo) {
         Set<ConstraintViolation<ProcessoNotificacao>> violations;
 
-        if(processo.getHistorico().isEmpty()){
+        if (processo.getHistorico().isEmpty()) {
             //validações do novo processo.
-            if(processo.getId() != null){
+            if (processo.getId() != null) {
                 //O processo necessita de ID nulo na primeira tentativa de salvar
                 violations = validator.validate(processo, NovaNotificacao.class);
             }
         } else {
             //O processo já foi salvo ao menos uma vez
-            if(processo.getId() == null){
+            if (processo.getId() == null) {
                 //O processo necessita ter um id caso já exista.
                 violations = validator.validate(processo, NotificacaoSalva.class);
-            } else{
-                if(processo.getObito().getPaciente().getDocumentoSocial().getTipoDocumentoComFoto().equals("PNI")){
+            } else {
+                if (processo.getObito().getPaciente().getDocumentoSocial().getTipoDocumentoComFoto() == PNI) {
                     violations = validator.validate(processo, ObitoPNI.class);
                 }
-                if(processo.getObito().getCorpoEncaminhamento().equals("NAO_ENCAMINHADO")){
+                if (processo.getObito().getCorpoEncaminhamento() == NAO_ENCAMINHADO) {
                     violations = validator.validate(processo, ObitoNaoEncaminhado.class);
                 } else {
                     violations = validator.validate(processo, ObitoEncaminhado.class);
                 }
-                if(processo.getObito().isAptoDoacao()){
+                if (processo.getObito().isAptoDoacao()) {
                     violations = validator.validate(processo, ObitoApto.class);
                 } else {
                     violations = validator.validate(processo, ObitoInapto.class);
